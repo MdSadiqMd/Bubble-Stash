4e47d517c69f6ff61830be3900b4a00b585cf591
Md.Sadiq <mohammadsadiq4950@gmail.com>
2025-08-12 22:49:19 +0530

    🪵  added loading of secrets and retriving them logic

--- internal/vault/vault.go
+++ internal/vault/vault.go
@@ -1,42 +1,115 @@
 package vault
 
 import (
+	"encoding/json"
 	"errors"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+	"sync"
 
 	"github.com/MdSadiqMd/Bubble-Stash/internal/encrypt"
 )
 
 type Vault struct {
+	mutex       sync.Mutex
 	encodingKey string
+	filePath    string
 	keyValues   map[string]string
 }
 
-func Memory(encodingKey string) Vault {
-	return Vault{
+func File(encodingKey, filePath string) *Vault {
+	return &Vault{
 		encodingKey: encodingKey,
-		keyValues:   make(map[string]string),
+		filePath:    filePath,
 	}
 }
 
-func (v *Vault) Get(key string) (string, error) {
-	encryptedValue, ok := v.keyValues[key]
-	if !ok {
-		return "", errors.New("vault: no value found for that")
-	}
-
-	decryptedValue, err := encrypt.Decrypt(v.encodingKey, encryptedValue)
+func (v *Vault) loadKeyValues() error {
+	f, err := os.Open(v.filePath)
 	if err != nil {
-		return "", err
+		v.keyValues = make(map[string]string)
+		return err
 	}
-	return decryptedValue, nil
-}
+	defer f.Close()
 
-func (v *Vault) Set(key, value string) error {
-	encryptedValue, err := encrypt.Encrypt(v.encodingKey, value)
+	var sb strings.Builder
+	_, err = io.Copy(&sb, f)
 	if err != nil {
 		return err
 	}
 
-	v.keyValues[key] = encryptedValue
+	decryptedJSON, err := encrypt.Decrypt(v.encodingKey, sb.String())
+	if err != nil {
+		return err
+	}
+
+	r := strings.NewReader(decryptedJSON)
+	decode := json.NewDecoder(r)
+	err = decode.Decode(&v.keyValues)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (v *Vault) saveKeyValues() error {
+	var sb strings.Builder
+	enc := json.NewEncoder(&sb)
+	err := enc.Encode(v.keyValues)
+	if err != nil {
+		return err
+	}
+
+	encryptedJSON, err := encrypt.Encrypt(v.encodingKey, sb.String())
+	if err != nil {
+		return err
+	}
+
+	f, err := os.OpenFile(v.filePath, os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	_, err = fmt.Fprint(f, encryptedJSON)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (v *Vault) Get(key string) (string, error) {
+	v.mutex.Lock()
+	defer v.mutex.Unlock()
+
+	err := v.loadKeyValues()
+	if err != nil {
+		return "", err
+	}
+
+	value, ok := v.keyValues[key]
+	if !ok {
+		return "", errors.New("vault: no value found for that")
+	}
+	return value, nil
+}
+
+func (v *Vault) Set(key, value string) error {
+	v.mutex.Lock()
+	defer v.mutex.Unlock()
+
+	err := v.loadKeyValues()
+	if err != nil {
+		return err
+	}
+
+	v.keyValues[key] = value
+
+	err = v.saveKeyValues()
+	if err != nil {
+		return err
+	}
 	return nil
 }
